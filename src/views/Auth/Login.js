447f1ab3a6767b379316dffeea3a5cb590a5b4f4
amir hossein rezayy <ah.rezayy@chmail.ir>
2022-05-09 18:23:30 +0430

    feat: add login page

--- src/views/Auth/Login.js
+++ src/views/Auth/Login.js
@@ -1,18 +1,50 @@
 import { useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
+import { styled } from '@mui/material/styles'
+
+import TextField from '@mui/material/TextField'
+import Button from '@mui/material/Button'
+import Box from '@mui/material/Box'
 
 import { LOGIN } from 'src/store/actions.js'
+
+const LoginWrapper = styled(Box)(() => ({
+  position: 'relative',
+  marginTop: '20%',
+  display: 'flex',
+  gap: '32px',
+  flexDirection: 'column',
+  alignItems: 'center'
+}))
 
 function Login() {
   const dispatch = useDispatch()
   const navigate = useNavigate()
 
-  const login = () => {
+  const login = (e) => {
+    e.preventDefault()
     dispatch({ type: LOGIN })
     navigate('/', { replace: true })
   }
 
-  return <button type="button" onClick={() => login()}>Login</button>
+  return (
+    <form onSubmit={login}>
+      <LoginWrapper>
+        <div>LOGIN</div>
+        <TextField
+          // required
+          size="small"
+          label="user name"
+        />
+        <TextField
+          // required
+          size="small"
+          label="password"
+        />
+        <Button type="submit" variant="outlined">LOGIN</Button>
+      </LoginWrapper>
+    </form>
+  )
 }
 
 export default Login
